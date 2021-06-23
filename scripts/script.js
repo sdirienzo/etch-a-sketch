1dc5fbe0ea6852194140e1c5564fce2e954cf223
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-06-22 21:14:49 -0400

    Introduce grid coloring function

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
+const DEFAULT_GRID_ROWS = 16;
+const DEFAULT_GRID_COLUMNS = 16;
+const CELL_CLASS_NAME = "cell";
+const container = document.getElementsByClassName("container")[0];
+
 function clearGrid(container) {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+    return;
 }
 
 function createGrid(container, rows, columns) {
@@ -14,6 +20,32 @@ function createGrid(container, rows, columns) {
         newDiv.classList.add("cell");
         container.appendChild(newDiv);
     }
+
+    addHoverEventListenerToCells(getCells(CELL_CLASS_NAME));
+    
+    return;
 }
 
-let container = document.getElementsByClassName("container")[0];
+function getCells(className) {
+    return document.getElementsByClassName(className);
+}
+
+function colorGrid(cellNodeList) {
+    for (let index = 0; index < cellNodeList.length; index++) {
+        let cell = cellNodeList[index];
+        cell.addEventListener("mouseover", function(event) {
+            cell.style.backgroundColor = "black";
+            return;
+        });
+    }
+    return;
+}
+
+//Init default grid on page load
+createGrid(container, DEFAULT_GRID_ROWS, DEFAULT_GRID_COLUMNS);
+
+
+
+
+
+
